9c1936e5fdbcc0108dd0c704200337791e84c388
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-05 13:57:24 +0200

    feat: getting some data JESUS THANK YOU LORD GOD ALMIGHTY ALLELUJAH

--- src/run.py
+++ src/run.py
@@ -1,10 +1,25 @@
 from data import PLAN_LISTS_WITH_BOOK_NUMBERS as lists_data
+from data import BIBLE_BOOK_NUMBER_TO_NUMBER_OF_CHAPTERS as chapter_counts
 
 def get_next_reading_for_list(
-    current_list_index:int,
-    current_Book_number:int,
-    current_chapter_number:int
+    list_index:int,
+    Book_index:int,
+    chapter_number:int
 ):
-    print(lists_data[current_list_index])
+    # Get data with Book numbers for currently selected Bible reading list
+    current_list_data=lists_data[list_index]
+
+    # Get a Book number from a selected Book index
+    selected_Book_number = current_list_data[Book_index]
+
+    # Get a number of existing chapters for currently selected Bible Book
+    available_chapters = chapter_counts[selected_Book_number]
+
+    print(current_list_data)
+    print(selected_Book_number)
+    print(available_chapters)
+
+    if chapter_number < available_chapters:
+        chapter_number += 1
 
 get_next_reading_for_list(0,0,0)
