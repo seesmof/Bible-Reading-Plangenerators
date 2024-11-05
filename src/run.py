d7731488af78ecbf129b3686415be8ecd7585f17
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-05 14:02:09 +0200

    feat: reading list switching algorithm

--- src/run.py
+++ src/run.py
@@ -19,7 +19,22 @@ def get_next_reading_for_list(
     print(selected_Book_number)
     print(available_chapters)
 
+    # If there are more Chapters to be read in this Book
     if chapter_number < available_chapters:
+        # Simply increment our chapter while we can
         chapter_number += 1
+
+    # If current chapter exceeds available chapters for the current Bible Book
+    else:
+        # Reset the chapter number back to one since we will be switching to a next Book on a list
+        chapter_number = 1
+
+        # First check if there are any more Books further in a list
+        if Book_index < len(current_list_data)-1:
+            # Then we simply switch to the next Book in a list 
+            Book_index += 1
+        else:
+            # If there are no more Book to read further in a list, then we restart our list
+            Book_index = 0
 
 get_next_reading_for_list(0,0,0)
