6c50b142e70eb860f7b6bb9a6c0594a53e405e99
Oleg Onyshchenko <seesmwork@gmail.com>
2026-08-01 12:23:27 +0300

    fail to figure it out

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -1,7 +1,14 @@
 import { HornerData } from "./data";
 
 export const calculateDay = (day: number) => {
-  for (const [key, value] of Object.entries(HornerData)) {
-    console.log(key);
+  for (const [category, bookIds] of Object.entries(HornerData)) {
+    console.log(category);
   }
 };
+
+/* 
+Map to each list and do something, im dumb
+You go to Genesis, first is chatper 1
+Then you go to History and Joshua, chapter 1
+For each category you ... i dont know
+*/
--- src/Old_Testament_Consecutive.py
+++ src/Old_Testament_Consecutive.py
@@ -2,26 +2,30 @@ import os
 import util
 from util.const import BIBLE_BOOK_NUMBER_TO_NUMBER_OF_CHAPTERS as chapter_numbers
 from util.const import BIBLE_BOOK_NUMBER_TO_KULISH_BIBLE_NAME as Book_names
-from Grant_Horner_Bible_Reading_Plan import get_Bolls_reading_link, get_eBible_reading_link
+from Grant_Horner_Bible_Reading_Plan import (
+    get_Bolls_reading_link,
+    get_eBible_reading_link,
+)
 
-res=[]
-for B in range(1,40):
-    Bn=Book_names[B]
-    for c in range(1,chapter_numbers[B]+1):
-        l=get_Bolls_reading_link(B,c)
-        t=f'[{Bn} {c}]({l})'
+res = []
+for B in range(1, 40):
+    Bn = Book_names[B]
+    for c in range(1, chapter_numbers[B] + 1):
+        l = get_eBible_reading_link(B, c)
+        t = f"[{Bn} {c}]({l})"
         res.append(t)
 
-header='''---
+header = """---
 aliases:
   - СЗ
   - OT
   - Old Testament
 ---
-'''
-target_file=os.path.join(util.results_folder_path,'OT.md')
+"""
+target_file = os.path.join(util.results_folder_path, "OT.md")
 try:
-    with open(target_file,encoding='utf-8',mode='w') as f:
+    with open(target_file, encoding="utf-8", mode="w") as f:
         f.write(header)
-        f.write('\n'.join(res))
-except: pass
+        f.write("\n".join(res))
+except:
+    pass
